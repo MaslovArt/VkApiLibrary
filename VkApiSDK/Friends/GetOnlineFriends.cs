9dc4b09141ed835ea440eca7059f2eb64247ae4f
Maslov <artemiy_maslov@mail.ru>
2019-02-22 23:57:50 +0300

    Добавлен класс запроса списка онлайн друзей. Добавлен базовый класс вк ответов сервера. Добавлен способ авторизации для винформс. Изменен тип айди с инт на стринг. Удалены ненужные ссылки.

--- VkApiSDK/Friends/GetOnlineFriends.cs
+++ VkApiSDK/Friends/GetOnlineFriends.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VkApiSDK.Friends
+﻿namespace VkApiSDK.Friends
 {
     public class GetOnlineFriends : GetFriends
     {
