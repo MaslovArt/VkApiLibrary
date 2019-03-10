e3c6e860b3018928512b35f805a49fe5f23cdfef
Maslov <artemiy_maslov@mail.ru>
2019-03-10 12:13:55 +0300

    Добавлен метод счетчиков пользователя

---
+++ VkApiLibrary/Account/AccountFilter.cs
@@ -0,0 +1,50 @@
+﻿namespace VkApiSDK.Account
+{
+    public static class AccountFilter
+    {
+        /// <summary>
+        /// Новые заявки в друзья
+        /// </summary>
+        public static string Friends = "friends";
+        /// <summary>
+        /// Предлагаемые друзья
+        /// </summary>
+        public static string FriendsSuggestions = "friends_suggestions"; 
+        /// <summary>
+        /// Новые сообщения
+        /// </summary>
+        public static string Messages = "messages"; 
+        /// <summary>
+        /// Новые отметки на фотографиях
+        /// </summary>
+        public static string Photos = "photos"; 
+        /// <summary>
+        /// Новые отметки на видеозаписях
+        /// </summary>
+        public static string Videos = "videos";
+        /// <summary>
+        /// Подарки
+        /// </summary>
+        public static string Gifts = "gifts"; 
+        /// <summary>
+        /// События
+        /// </summary>
+        public static string Events = "events"; 
+        /// <summary>
+        /// Сообщества
+        /// </summary>
+        public static string Groups = "groups"; 
+        /// <summary>
+        /// Ответы
+        /// </summary>
+        public static string Notifications = "notifications"; 
+        /// <summary>
+        /// Запросы в мобильных играх
+        /// </summary>
+        public static string SDK = "sdk"; 
+        /// <summary>
+        /// Уведомления от приложений
+        /// </summary>
+        public static string AppRequests = "app_requests"; 
+    }
+}
---
+++ VkApiLibrary/Account/Methods/GetCounters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkApiSDK.Abstraction;
+
+namespace VkApiSDK.Account.Methods
+{
+    /// <summary>
+    /// Возвращает ненулевые значения счетчиков пользователя.
+    /// </summary>
+    public class GetCounters : VkApiMethod
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <c>GetCounters</c>
+        /// </summary>
+        /// <param name="AccessToken">Токен доступа</param>
+        /// <param name="Filters">Счетчики, информацию о которых нужно вернуть</param>
+        public GetCounters(string AccessToken, IEnumerable<string> Filters)
+            :base(AccessToken)
+        {
+            VkApiMethodName = "account.getCounters";
+            this.Filters = Filters;
+        }
+
+        /// <summary>
+        /// Счетчики, информацию о которых нужно вернуть
+        /// </summary>
+        public IEnumerable<string> Filters { get; set; }
+
+        protected override string GetMethodApiParams()
+        {
+            return string.Format("&filter={0}", ArrayToString(Filters));
+        }
+    }
+}
